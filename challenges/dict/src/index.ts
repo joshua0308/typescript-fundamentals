3749b69088e4f21deed634f6dc35e6c8f66018ba
Josh Kim <joshua940308@gmail.com>
2019-12-06 22:07:20 +0900

    Complete dictionary challenge

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,36 @@
-export type Dict = {};
+export type Dict<T> = {
+  [k: string]: T | undefined;
+};
 
+// dict =
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(dict: Dict<T>, fn: (arg: T, idx: number) => S): Dict<S> {
+  const out: Dict<S> = {};
+
+  // iterate through the dictionary
+  Object.keys(dict).forEach((key, idx) => {
+    const thisItem = dict[key];
+    if (typeof thisItem !== 'undefined') {
+      out[key] = fn(thisItem, idx);
+    }
+  });
+  return out;
+}
+
+const testDict = {
+  typescript: ['ts'],
+  html: ['html', 'htm']
+};
+
+mapDict(testDict, exts => exts.map(e => `*.${exts}`).join(', '));
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<T, S>(dict: Dict<T>, reducer: (acc: S, cur: T, idx: number) => S, initialVal: S) {
+  let acc: S = initialVal;
+  Object.keys(dict).forEach((key, idx) => {
+    const thisItem = dict[key];
+    if (typeof thisItem !== 'undefined') acc = reducer(acc, thisItem, idx);
+  });
+
+  return acc;
+}
